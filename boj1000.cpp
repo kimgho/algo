6ae12468502489387ea26a917cd0ef0f857af7ed
kimgho <kgh67811@naver.com>
2025-01-05 23:58:43 +0900

    백준(클래스1)

---
+++ boj1000.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+int n, m;
+int main()
+{
+    io;
+    cin >> n >> m;
+    cout << n + m << "\n";
+    return 0;
+}
